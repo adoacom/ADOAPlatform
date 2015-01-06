b8baf92d4fe0c53e457617d98da2c9e55ea231f3
Huynh Tuan Cuong <htcuong@brights.vn>
2015-01-07 00:23:27 +0700

    #4 html
    fix some html

--- adoa/js/portfolio.js
+++ adoa/js/portfolio.js
@@ -21,6 +21,16 @@ $(window).load(function(){
 		$("#list-team").isotope({ filter: selector });
 		return false;			
 	});	
+	
+	
+	$("#list-partners").isotope();	 
+	$('#partners-filter a').click(function(){	
+		$('#partners-filter li').removeClass('active');
+		$(this).parent('li').addClass('active');
+		var selector = $(this).attr('data-filter');							
+		$("#list-partners").isotope({ filter: selector });
+		return false;			
+	});	
 
 });
 
